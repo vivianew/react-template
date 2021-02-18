85fec201d440e61d826de1b538b1fd7a0ae006c4
Viviane Wu <vivianewu92@gmail.com>
2021-02-18 23:41:32 +0800

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Route, HashRouter as Router } from 'react-router-dom';
+import LoginContainer from './containers/LoginContainer/LoginContainer';
 import './app.scss';
 
 const App = () => {
   return (
-    <div className="app__title">
-      Hello World
-    </div>
-  );
+    <Router>
+      <Route path="/" component={LoginContainer} />
+    </Router>
+  )
 };
 
 export default App;
---
+++ src/containers/LoginContainer/LoginContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginContainer = () => {
+  return (
+    <div>hello World</div>
+  )
+};
+
+export default LoginContainer;
