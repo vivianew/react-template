e2e55f7eb080151e86d10dad1c556c40aa678155
Viviane Wu <vivianewu92@gmail.com>
2021-02-18 17:21:57 +0800

    add babel polyfill to webpack config

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -3,7 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.js'),
+  entry: ['core-js/stable', 'regenerator-runtime/runtime', path.resolve(__dirname, '..', './src/index.js')],
   module: {
     rules: [
       {
